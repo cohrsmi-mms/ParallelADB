d49f1cd3b246a9088210cef30e7758de211f422e
cohrs <cohrs@xplace.de>
2026-05-12 18:14:30 +0200

    add optional 'root' parameter

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Enable adb command parallel run in all connected devices.
 
 Usage
 -------
-Use `parallel-connect.py [text file containing newline-separated IP addresses]` to connect to devices in parallel.
+Use `parallel-connect.py {text file containing newline-separated IP addresses} [root]` to connect to devices in parallel. Parameter 'root' is optional.
 
 Adapt and use `parallel-cmd.py` to run adb commands in parallel.
--- parallel-connect.py
+++ parallel-connect.py
@@ -2,15 +2,24 @@
 import sys
 from paralleladb import ParallelADB
 
+root = False
 if len(sys.argv) > 1:
     csvFile = sys.argv[1]
 else:
-    print("Usage: " + sys.argv[0] + " [text file containing newline-separated ip addresses]")
+    print("Usage: " + sys.argv[0] + " {text file containing newline-separated ip addresses} [root]")
     exit(1)
+if len(sys.argv) > 2 and sys.argv[2] == "root":
+    root = True
 
 outputs = ParallelADB.run(cmd=None, csv=csvFile)
-
 for o in outputs:
     print(o.serial + ": ")
     for r in o.results:
         print(r.replace("\n", ""))
+
+if root:
+    outputs = ParallelADB.run("root", is_shell_cmd=False)
+    for o in outputs:
+        print(o.serial + ": ")
+        for r in o.results:
+            print(r.replace("\n", ""))
